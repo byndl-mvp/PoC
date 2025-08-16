93573449f8d4f64e94e319f7123af652e9a01e54
byndl-mvp <christoph.keilbar@byndl.de>
2025-08-16 15:32:33 +0200

    Update db.js

--- backend/db.js
+++ backend/db.js
@@ -2,11 +2,19 @@ const { Pool } = require('pg');
 
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
-  ssl: { rejectUnauthorized: false }, // Render-Postgres verlangt SSL
+  ssl: process.env.DATABASE_SSL === 'true'
+    ? { rejectUnauthorized: false }
+    : false,
 });
 
 async function query(text, params) {
-  return pool.query(text, params);
+  const client = await pool.connect();
+  try {
+    const res = await client.query(text, params);
+    return res;
+  } finally {
+    client.release();
+  }
 }
 
 module.exports = { query };
