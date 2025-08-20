53c592e6c7f3a8842f67e273d98103f9ad64121d
byndl-mvp <christoph.keilbar@byndl.de>
2025-08-20 12:33:06 +0200

    Update tailwind.config.js

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,11 +1,11 @@
 /** @type {import('tailwindcss').Config} */
-export default {
+module.exports = {
   content: [
-    './index.html',
-    './src/**/*.{js,jsx,ts,tsx}',
+    "./src/**/*.{js,jsx,ts,tsx}",
+    "./public/index.html"
   ],
   theme: {
     extend: {},
   },
   plugins: [],
-};
+}
