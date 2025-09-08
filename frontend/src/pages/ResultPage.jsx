1a8fa361b6f1742dc5db0c4a0f4bf80229f9fa5b
byndl-mvp <christoph.keilbar@byndl.de>
2025-09-08 13:31:53 +0200

    Update ResultPage.jsx

--- frontend/src/pages/ResultPage.jsx
+++ frontend/src/pages/ResultPage.jsx
@@ -542,7 +542,7 @@ const handleSave = async () => {
                   <input
                     type="number"
                     name="quantity"
-                    step="0.01"
+                    step="0.10"
                     className="w-full border border-gray-300 rounded-lg px-4 py-2"
                     defaultValue={selectedPosition.quantity}
                   />
@@ -563,7 +563,7 @@ const handleSave = async () => {
                   <input
                     type="number"
                     name="unitPrice"
-                    step="0.01"
+                    step="0.10"
                     className="w-full border border-gray-300 rounded-lg px-4 py-2"
                     defaultValue={selectedPosition.unitPrice}
                   />
