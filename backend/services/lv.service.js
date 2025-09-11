2208aeeb3ec9c4879e172335ea737e5e111e230e
byndl-mvp <christoph.keilbar@byndl.de>
2025-09-11 11:33:06 +0200

    Update lv.service.js

--- backend/services/lv.service.js
+++ backend/services/lv.service.js
@@ -1,1 +1,48 @@
+const { llmWithPolicy } = require('./llm.service');
+const db = require('./database.service');
+const { formatCurrency, parseFensterMaße } = require('../utils/helpers');
 
+class LVService {
+  
+  /**
+   * Generiert detailliertes LV mit Mengenberechnung
+   */
+  async generateDetailedLV(projectId, tradeId) {
+    // KOPIEREN SIE aus server.js (ca. Zeilen 1230-2100)
+    // Die KOMPLETTE generateDetailedLV Funktion
+  }
+  
+  /**
+   * Validiert und korrigiert Preise im LV
+   */
+  validateAndFixPrices(lv, tradeCode) {
+    // KOPIEREN SIE aus server.js (ca. Zeilen 2200-2450)
+    // Die KOMPLETTE validateAndFixPrices Funktion
+  }
+  
+  /**
+   * Finale LV-Validierung
+   */
+  finalLVValidation(lv, tradeCode) {
+    // KOPIEREN SIE aus server.js
+    // Die KOMPLETTE finalLVValidation Funktion
+  }
+  
+  /**
+   * Prüft auf doppelte Positionen
+   */
+  checkForDuplicatePositions(lv) {
+    // KOPIEREN SIE aus server.js
+    // Die KOMPLETTE checkForDuplicatePositions Funktion
+  }
+  
+  /**
+   * Berechnet LV-Summen
+   */
+  calculateLVSummary(lv) {
+    // KOPIEREN SIE aus server.js
+    // Die KOMPLETTE calculateLVSummary Funktion
+  }
+}
+
+module.exports = new LVService();
