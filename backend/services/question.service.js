593470c8661fcfb76d591d5ba26b6a285d748ec9
byndl-mvp <christoph.keilbar@byndl.de>
2025-09-11 11:19:21 +0200

    Update question.service.js

--- backend/services/question.service.js
+++ backend/services/question.service.js
@@ -1,1 +1,36 @@
+const { TRADE_COMPLEXITY, DEFAULT_COMPLEXITY } = require('../config/constants');
+const { llmWithPolicy } = require('./llm.service');
+const db = require('./database.service');
 
+class QuestionService {
+  
+  /**
+   * Intelligente, dynamische Fragenanzahl-Ermittlung
+   */
+  getIntelligentQuestionCount(tradeCode, projectContext, intakeAnswers = []) {
+    // KOPIEREN SIE DIE KOMPLETTE FUNKTION aus Ihrem Dokument (Zeilen 4-344)
+  }
+  
+  /**
+   * Intelligente Antwort-Validierung und Schätzung
+   */
+  async validateAndEstimateAnswers(answers, tradeCode, projectContext) {
+    // KOPIEREN SIE DIE KOMPLETTE FUNKTION aus server.js
+  }
+  
+  /**
+   * Intelligente Fragengenerierung mit Mengenerfassung
+   */
+  async generateQuestions(tradeId, projectContext = {}) {
+    // KOPIEREN SIE DIE KOMPLETTE FUNKTION aus server.js (sehr lang!)
+  }
+  
+  /**
+   * Generiert adaptive Folgefragen basierend auf Kontext-Antwort
+   */
+  async generateContextBasedQuestions(tradeId, projectId, contextAnswer) {
+    // KOPIEREN SIE DIE KOMPLETTE FUNKTION aus server.js
+  }
+}
+
+module.exports = new QuestionService();
