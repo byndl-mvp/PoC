5f2ccd791379a97240d743bcc0006dccea6f2e87
byndl-mvp <christoph.keilbar@byndl.de>
2025-10-29 19:28:48 +0100

    Update LVPreviewPage.jsx

--- frontend/src/pages/LVPreviewPage.jsx
+++ frontend/src/pages/LVPreviewPage.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, Fragment } from 'react';
+import React, { useState, useEffect } from 'react';
 import { useParams, useNavigate } from 'react-router-dom';
 import { apiUrl } from '../api';
 
