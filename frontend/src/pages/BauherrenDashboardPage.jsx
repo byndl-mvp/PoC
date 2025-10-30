d3e70f3c3bbf89c78c2cd70bd7727fad644262e7
byndl-mvp <christoph.keilbar@byndl.de>
2025-10-30 16:37:24 +0100

    Update BauherrenDashboardPage.jsx

--- frontend/src/pages/BauherrenDashboardPage.jsx
+++ frontend/src/pages/BauherrenDashboardPage.jsx
@@ -3,6 +3,7 @@ import { useNavigate, Link } from 'react-router-dom';
 import { apiUrl } from '../api';
 import NotificationCenter from '../pages/NotificationCenter';
 import MessageCenter from '../pages/MessageCenter';
+import { OfferEvaluationModal, OfferComparisonModal } from '../pages/OfferEvaluationModals';
 
 function formatCurrency(value) {
   if (!value && value !== 0) return '0 €';
