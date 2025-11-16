d7de6386c7c0ce08a714e193feedf11820d35473
byndl-mvp <christoph.keilbar@byndl.de>
2025-11-16 14:43:33 +0100

    Update HandwerkerDashboardPage.jsx

--- frontend/src/pages/HandwerkerDashboardPage.jsx
+++ frontend/src/pages/HandwerkerDashboardPage.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import { useNavigate, Link } from 'react-router-dom';
 import { apiUrl } from '../api';
 import NotificationCenter from '../pages/NotificationCenter';
