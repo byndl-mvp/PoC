d6ed554da04f9e4d856d7be358520585d0ec37e7
byndl-mvp <christoph.keilbar@byndl.de>
2025-11-16 14:24:57 +0100

    Update BauherrenDashboardPage.jsx

--- frontend/src/pages/BauherrenDashboardPage.jsx
+++ frontend/src/pages/BauherrenDashboardPage.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useCallback } from 'react';
+import React, { useState, useEffect, useCallback, useRef } from 'react';
 import { useNavigate, Link } from 'react-router-dom';
 import { apiUrl } from '../api';
 import NotificationCenter from '../pages/NotificationCenter';
