8f2b55a52b659ae0f4e0e5380120bdd9cb2fe79d
byndl-mvp <christoph.keilbar@byndl.de>
2025-11-18 14:05:55 +0100

    Update ResultPage.jsx

--- frontend/src/pages/ResultPage.jsx
+++ frontend/src/pages/ResultPage.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState, useRef } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useParams, Link } from 'react-router-dom';
 import { apiUrl } from '../api';
 import { useNavigate } from 'react-router-dom';
