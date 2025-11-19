045e91307ae8673c7038f8504e8c7995d185162b
byndl-mvp <christoph.keilbar@byndl.de>
2025-11-19 22:03:47 +0100

    Update HandwerkerNachtraegeUebersichtPage.jsx

--- frontend/src/pages/HandwerkerNachtraegeUebersichtPage.jsx
+++ frontend/src/pages/HandwerkerNachtraegeUebersichtPage.jsx
@@ -272,7 +272,6 @@ export default function HandwerkerNachtraegeUebersichtPage() {
                         </button>
                       </div>
                     </div>
-                </div>
               ))}
             </div>
           </div>
