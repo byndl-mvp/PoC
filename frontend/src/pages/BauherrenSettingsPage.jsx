c86357500b07e42a5476076afb83bd256ed0b10a
byndl-mvp <christoph.keilbar@byndl.de>
2025-11-27 11:58:56 +0100

    Update BauherrenSettingsPage.jsx

--- frontend/src/pages/BauherrenSettingsPage.jsx
+++ frontend/src/pages/BauherrenSettingsPage.jsx
@@ -42,7 +42,6 @@ export default function BauherrenSettingsPage() {
   const [newPassword, setNewPassword] = useState('');
   const [confirmPassword, setConfirmPassword] = useState('');
   const [currentPassword, setCurrentPassword] = useState('');
-  const [twoFactorEnabled, setTwoFactorEnabled] = useState(false); // eslint-disable-line react-hooks/exhaustive-deps
   const [emailVerified, setEmailVerified] = useState(false);
   const [createdAt, setCreatedAt] = useState(null);
   const [feedbackText, setFeedbackText] = useState('');
