41a7b15933989f29ec77939beee4f6c37057cdb9
byndl-mvp <christoph.keilbar@byndl.de>
2025-11-30 22:11:06 +0100

    Update LandingPage.jsx

--- frontend/src/pages/LandingPage.jsx
+++ frontend/src/pages/LandingPage.jsx
@@ -45,7 +45,7 @@ export default function LandingPage() {
               <svg className="w-4 h-4 sm:w-5 sm:h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24">
                 <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6"/>
               </svg>
-              <span className="hidden xs:inline sm:inline">Bauherren-</span><span>Login</span>
+              <span>Bauherren-Login</span>
             </Link>
             <Link
               to="/handwerker/login"
@@ -54,7 +54,7 @@ export default function LandingPage() {
               <svg className="w-4 h-4 sm:w-5 sm:h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24">
                 <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M21 13.255A23.931 23.931 0 0112 15c-3.183 0-6.22-.62-9-1.745M16 6V4a2 2 0 00-2-2h-4a2 2 0 00-2 2v2m4 6h.01M5 20h14a2 2 0 002-2V8a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z"/>
               </svg>
-              <span className="hidden xs:inline sm:inline">Handwerker-</span><span>Login</span>
+              <span>Handwerker-Login</span>
             </Link>
           </div>
         </div>
